618c0dab9f9b42a8460df9554c3af705c8efb920
Zain <zainhussaini9898@gmail.com>
2024-12-24 04:21:57 +0530

    10 commit

--- src/components/useLetterTransforms.jsx
+++ src/components/useLetterTransforms.jsx
@@ -2,11 +2,15 @@ import { useMemo } from "react";
 import { useTransform } from "framer-motion";
 
 export function useLetterTransforms(scrollYProgress, word) {
+    // Generate random transforms for each letter once
     const transforms = useMemo(() => {
         return word.split("").map(() => {
             return Math.floor(Math.random() * -75) - 25;
         });
-    }, [scrollYProgress, word]);
+    }, [word]); // Only word changes need to trigger recomputation
 
-    return transforms.map(transform => useTransform(scrollYProgress, [0, 1], [0, transform]));
+    // Apply the `useTransform` hook for each letter's transform
+    return transforms.map((transform) =>
+        useTransform(scrollYProgress, [0, 1], [0, transform])
+    );
 }
