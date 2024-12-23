d4b7b37f2cc42f5f3f84bcba98a4a91d5a610525
Zain <zainhussaini9898@gmail.com>
2024-12-24 04:19:06 +0530

    9 commit

--- src/components/useLetterTransforms.jsx
+++ src/components/useLetterTransforms.jsx
@@ -2,9 +2,11 @@ import { useMemo } from "react";
 import { useTransform } from "framer-motion";
 
 export function useLetterTransforms(scrollYProgress, word) {
-    return useMemo(() => {
-        return word.split("").map(() =>
-            useTransform(scrollYProgress, [0, 1], [0, Math.floor(Math.random() * -75) - 25])
-        );
+    const transforms = useMemo(() => {
+        return word.split("").map(() => {
+            return Math.floor(Math.random() * -75) - 25;
+        });
     }, [scrollYProgress, word]);
+
+    return transforms.map(transform => useTransform(scrollYProgress, [0, 1], [0, transform]));
 }
